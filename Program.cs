59dcfc150621f033aa4bf6aa27756612d5f673ae
Александр <berzerk54@gmail.com>
2021-03-16 21:57:03 +0700

    Lesson 2 - procedure basics

--- Program.cs
+++ Program.cs
@@ -10,8 +10,28 @@ namespace sharpsnake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("helloworld");
+            int x1 = 1;
+            int y1 = 3;
+            int x2 = 4;
+            int y2 = 5;
+            
+            char sym1 = '*';
+            char sym2 = '#';
+
+
+            Draw(x1, y1, sym1);
+            Draw(x2, y2, sym2);
+
+
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x,int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+
         }
     }
 }
