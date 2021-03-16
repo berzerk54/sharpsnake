5f756576dd194df6df9d14d06539763adf983682
Александр <berzerk54@gmail.com>
2021-03-16 22:15:25 +0700

    +Class point

--- Program.cs
+++ Program.cs
@@ -7,31 +7,21 @@ using System.Threading.Tasks;
 namespace sharpsnake
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            int x2 = 4;
-            int y2 = 5;
-            
-            char sym1 = '*';
-            char sym2 = '#';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
 
-
-            Draw(x1, y1, sym1);
-            Draw(x2, y2, sym2);
-
-
-
+            p1.Draw();
+            p2.Draw();
             Console.ReadLine();
-        }
-
-        static void Draw(int x,int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-
         }
     }
 }
